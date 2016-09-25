5d04f53edc7e3e03173c5a80be9538421f63545e
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2016-09-24 19:25:58 -0500

    modified CtoF.cpp file.

--- CtoF.cpp
+++ CtoF.cpp
@@ -6,21 +6,47 @@
 #include<iomanip>
 using namespace std;
 
+double C; // Celsius Variable
+double F; // Fahrenheit Variable
+char ans;
+
 int main()
 {
 
 
-    double C; // Celsius Variable
-    double F; // Fahrenheit Variable
+    //discribe to the user what the program does.
+    cout << "This program converts celsius to fahrenheit. Would you like to continue? (Y/N) ";
+    cin >> ans;
 
-    //Asking the user for celsius temperature.
-    cout << "What is your temperature in celsius? " << endl;
-    //receiving celsius input into C variable.
-    cin >> C;
-    // calculating celsius to fahrenheit.
-    F = 1.8 * C + 32;
-    // Displaying conversion from C to F.
-    cout << "\nYour temperature of " << C << " degree celsius" << " has been converted to: " << F << " degree fahrenheit" << endl << endl;
+
+    //if statement for whether user chooses yes or no.
+    if (ans == 'y'|| ans == 'Y'){
+
+        cout << "\nOk. What is your celsius temperature? ";
+
+        //receiving celsius temp.
+        cin >> C;
+
+        //Formatting setprecision if decimal answer occurs
+        cout << setprecision(5);
+
+        // calculating celsius to fahrenheit.
+        F = 1.8 * C + 32;
+
+        // Displaying conversion from C to F.
+        cout << endl << C << "°C" << " = " << F << "°F" << endl << endl;
+
+    //else if resonds to no answer
+    } else if(ans == 'n' || ans == 'N'){
+        cout << "\nSorry I couldn't help.\n" << endl;
+
+    //Else occurs when any key other than Y or N pressed
+    } else {
+        cout << "\nTry pressing either Y or N next time." << endl << endl;
+    }
+
+
+
 
     return 0;
 }
