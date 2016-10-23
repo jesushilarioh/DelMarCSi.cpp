037c07b1e2862c92c8e501195dea529d8b58323e
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2016-10-22 21:08:15 -0500

    added file.;

--- Chapter-6/overloaded-functions-with-user-input.cpp
+++ Chapter-6/overloaded-functions-with-user-input.cpp
@@ -16,42 +16,42 @@ double calcWeeklyPay(double);
 
 int main()
 {
-  char selection;   // Menu selection
-  int worked;       // Hours worked
-  double rate;      // Hourly pay rate
-  double yearly;    // Yearly salary
+    char selection;   // Menu selection
+    int worked;       // Hours worked
+    double rate;      // Hourly pay rate
+    double yearly;    // Yearly salary
 
-  // Set numeric output formatting.
-  cout << fixed << showpoint << setprecision(2);
+    // Set numeric output formatting.
+    cout << fixed << showpoint << setprecision(2);
 
-  // Display the menu and get a selections.
-  cout << "Do you want to calculate the weekly pay of\n";
-  cout << "(H) an hourly paid emplyee, or \n";
-  cout << "(S) a salaried employee?\n";
-  getChoice(selection);
+    // Display the menu and get a selections.
+    cout << "Do you want to calculate the weekly pay of\n";
+    cout << "(H) an hourly paid emplyee, or \n";
+    cout << "(S) a salaried employee?\n";
+    getChoice(selection);
 
-  // Process the menu selection.
-  switch (selection)
-  {
-    // Hourlypaid employees
-    case 'H' :
-    case 'h' : cout << "How many hours were worked? ";
-               cin >> worked;
-               cout << "What is the hourly pay rate? ";
-               cin >> rate;
-               cout << "The gross weekly pay is $";
-               cout << calcWeeklyPay(worked, rate) << endl;
-               break;
+    // Process the menu selection.
+    switch (selection)
+    {
+            // Hourlypaid employees
+        case 'H' :
+        case 'h' : cout << "How many hours were worked? ";
+            cin >> worked;
+            cout << "What is the hourly pay rate? ";
+            cin >> rate;
+            cout << "The gross weekly pay is $";
+            cout << calcWeeklyPay(worked, rate) << endl;
+            break;
 
-    // Salaried employee
-    case 'S' :
-    case 's' : cout "What is the annual salary? ";
-               cin >> yearly;
-               cout << "The gross weekly pay is $";
-               cout << calcWeeklyPay(yearly) << endl;
-               break;
-  }
-  return 0;
+            // Salaried employee
+        case 'S' :
+        case 's' : cout <<"What is the annual salary? ";
+            cin >> yearly;
+            cout << "The gross weekly pay is $";
+            cout << calcWeeklyPay(yearly) << endl;
+            break;
+    }
+    return 0;
 }
 
 //***********************************************************
@@ -63,17 +63,17 @@ int main()
 
 void getChoice(char & letter)
 {
-  // Get the user's selection.
-  cout << "Enter your choice (H or S): ";
-  cin >> letter;
+    // Get the user's selection.
+    cout << "Enter your choice (H or S): ";
+    cin >> letter;
 
-  // Validate the selection.
-  while (letter != 'H' && letter != 'h' &&
-         letter != 'S' && letter != 's')
-         {
-           cout << "Please enter H or S: ";
-           cin >> letter;
-         }
+    // Validate the selection.
+    while (letter != 'H' && letter != 'h' &&
+           letter != 'S' && letter != 's')
+    {
+        cout << "Please enter H or S: ";
+        cin >> letter;
+    }
 }
 
 //***********************************************************
@@ -86,7 +86,7 @@ void getChoice(char & letter)
 
 double calcWeeklyPay(int hours, double payRate)
 {
-  return hours * payRate;
+    return hours * payRate;
 }
 
 //***********************************************************
@@ -98,5 +98,5 @@ double calcWeeklyPay(int hours, double payRate)
 
 double calcWeeklyPay(double annSalary)
 {
-  return annSalary / 52;
+    return annSalary / 52;
 }
