e7003de2f10325b49a0ffd671b42655b8e6769cb
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2016-11-12 14:21:29 -0600

    experimenting with added shell files.

---
+++ gihtub.sh
@@ -0,0 +1,24 @@
+Last login: Sat Nov 12 10:21:49 on console
+Jesuss-iMac:~ hernandezfamily$ cd desktop
+Jesuss-iMac:desktop hernandezfamily$ cd github
+Jesuss-iMac:github hernandezfamily$ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
