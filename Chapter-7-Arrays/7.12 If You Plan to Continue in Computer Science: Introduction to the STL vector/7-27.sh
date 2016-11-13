36679059b0a4ea074849b62ab85d58612d69d372
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2016-11-13 12:52:04 -0600

    added 7-26.cpp and 7-27.cpp

---
+++ Chapter-7-Arrays/7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector/7-27.sh
@@ -0,0 +1,15 @@
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$ g++ -std=c++11 7-27.cpp
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$ ./a.out
+How many employees do you have? 3
+Enter the hours worked by 3 employees and their hourly rates.
+Hours worked by employee #1: 40
+Hourly pay rate for employee #1: 12.63
+Hours worked by employee #2: 25
+Hourly pay rate for employee #2: 10.35
+Hours worked by employee #3: 45
+Hourly pay rate for employee #3: 22.65
+Here is the gross pay for each employee:
+Employee #1: $505.20
+Employee #2: $258.75
+Employee #3: $1019.25
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$
