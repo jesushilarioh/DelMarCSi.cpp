4717c8c4cc4d0d1bcf5465d7795e184a9b8f1c73
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2016-11-20 17:48:53 -0600

    added programs that cover vector member functions including: empty, clear, pop_back, clear, and size.

---
+++ Chapter-7-Arrays/7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector/7-30.sh
@@ -0,0 +1,34 @@
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$ g++ -std=c++11 7-30.cpp
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$ ./a.out
+The values vector has 100 elements.
+I will call the clear member function...
+Now, the values vector has 0 elements.
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Chapter-7-Arrays/7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector/7-31.sh
@@ -0,0 +1,32 @@
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$ g++ -std=c++11 7-31.cpp
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$ ./a.out
+How many values do you wish to average? 5
+Enter a value: 12
+Enter a value: 13
+Enter a value: 6
+Enter a value: 7
+Enter a value: 8
+Average: 9
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$ g++ -std=c++11 7-31.cpp
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$ ./a.out
+How many values do you wish to average? 5
+Enter a value: 4
+Enter a value: 5
+Enter a value: 4
+Enter a value: 5
+Enter a value: 4
+Average: 4
+Jesuss-iMac:7.12 If You Plan to Continue in Computer Science: Introduction to the STL vector hernandezfamily$ 
+
+
+
+
+
+
+
+
+
+
+
+
+
