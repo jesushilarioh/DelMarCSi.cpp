dbbe575d927ac496c3a95aca61fb468042b85523
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2016-12-21 02:34:43 -0600

    added C++ files to Chapter 4 Making Decisions.

---
+++ Chapter-4-Making-Decisions/4.12 Comparing Characters and Strings/4-21.cpp
@@ -0,0 +1,9 @@
+//*************************************************************
+// This program uses relational operators to compare a string
+// entered by the user with valid stereo part numbers.
+//
+// By: Jesus Hilario Hernandez
+// Last Updated: December 20, 2016
+// Used from : Gaddis, "Starting Out With C++",
+//  8th Edition, Pg 197-198
+//*************************************************************
