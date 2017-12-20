d4105ec5acf8932ad60bd3e4d799db3f6143e30c
jesushilarioh <jesushilariohernandez@gmail.com>
2017-12-19 19:58:51 -0600

    modified Challenge 8 for chapter 2. December 19th 2017

--- Chapter-2-Intro-to-C++/Programming Challenges/08.cpp
+++ Chapter-2-Intro-to-C++/Programming Challenges/08.cpp
@@ -12,7 +12,8 @@
 *
 *       Write a program that holds the prices of the five items  in
 *       five variables. Display each item's price, the subtotal of the
-*       sale tax is 7%.
+*       sale, the amount of sales tax, and the total. Assume the sales
+*       tax is 7%.
 *
 * 	Jesus Hilario Hernandez
 * 	December 19th 2017
@@ -20,12 +21,31 @@
 *
 ********************************************************************/
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
+    float item1 = 15.95,
+          item2 = 24.95,
+          item3 = 6.95,
+          item4 = 12.95,
+          item5 = 3.95,
+          subtotal = item1 + item2 + item3 + item4 + item5,
+          totalSalesTax = subtotal * .07;
 
+    cout << endl;
+    cout << setprecision(2) << fixed;
+    cout << "Item 1 = $" << item1 << endl;
+    cout << "Item 2 = $" << item2 << endl;
+    cout << "Item 3 = $" << item3 << endl;
+    cout << "Item 4 = $" << item4 << endl;
+    cout << "Item 5 = $" << item5 << endl;
+    cout << "Subtotal = $" << subtotal << endl;
+    cout << "Sales tax = $" << totalSalesTax << endl;
+    cout << "Grand Total = $" << subtotal + totalSalesTax << endl;
+    cout << endl;
 
     return 0;
 }
