6c862e0efa79bf0f5eb9f046846615019f0889d7
jesushilarioh <jesushilariohernandez@gmail.com>
2017-12-19 21:03:25 -0600

    added challenge #11 on December 19th 2017.

--- Chapter-2-Intro-to-C++/Programming Challenges/10.cpp
+++ Chapter-2-Intro-to-C++/Programming Challenges/10.cpp
@@ -1,6 +1,6 @@
 /********************************************************************
 *
-*	09. TOTAL PURCHASE
+*	10. TOTAL PURCHASE
 *       A car holds 15 gallons of gasoline and can travel 375 miles
 *       before refueling. Write a program that calculates the number
 *       of miles per gallon the car gets. Display the result on the
